7286de0e8218a6e5313611246542879227ec81d3
0-0niknan0-0 <niknan202005@gmail.com>
2024-03-03 07:36:04 +0100

    Final changes

---
+++ src/js/1-gallery.js
@@ -0,0 +1,23 @@
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+
+console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryItemsHTML = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', galleryItemsHTML);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
